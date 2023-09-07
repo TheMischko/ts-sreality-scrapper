9f3aa4f2443119489eb401f639567e3f6f115e89
Michal_Dvorak <michal.dvorak999@gmail.com>
2023-09-07 14:02:33 +0200

    Backend: Add unit testing for flat service

---
+++ backend/tests/mocks/providers/MockFlatProvider.ts
@@ -0,0 +1,47 @@
+export const mockFlatProvider = () :IFlatProvider => {
+  const get = async () => {
+    return new Promise<Flat[]>((resolve, reject) => {
+      resolve([
+        {
+          id: 0,
+          title: "Byt 1+kk Praha Holešovice",
+          address: "Holešovice, Praha",
+          image_url: "https://images.com/image-123.jpg"
+        },
+        {
+          id: 1,
+          title: "Byt 3+kk Praha Smíchov",
+          address: "Smíchov, Praha",
+          image_url: "https://images.com/image-456.jpg"
+        },
+        {
+          id: 2,
+          title: "Byt 3+1 Praha Žižkov",
+          address: "Žižkov, Praha",
+          image_url: "https://images.com/image-423.jpg"
+        },
+        {
+          id: 3,
+          title: "Byt 2+kk Praha Vysočany",
+          address: "Vysočany, Praha",
+          image_url: "https://images.com/image-856.jpg"
+        },
+      ])
+    })
+  };
+
+  const getPart = async (pageIndex:number, pageSize:number) => {
+    return new Promise<Flat[]>(async (resolve, reject) => {
+      const flats = await get();
+      const startSliceIndex = pageIndex * pageSize;
+      const slice = flats.slice(startSliceIndex, startSliceIndex+pageSize);
+      resolve(slice);
+    })
+  
+  }
+
+  return {
+    get,
+    getPart
+  }
+}
