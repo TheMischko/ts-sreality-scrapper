93eec9168372f2d5458df9b0a15da3f18770d53f
Michal_Dvorak <michal.dvorak999@gmail.com>
2023-09-08 11:10:41 +0200

    Backend: Add page param to /flats route

--- backend/src/routers/flatsRouter.ts
+++ backend/src/routers/flatsRouter.ts
@@ -5,18 +5,33 @@ import database from "../database";
 
 const router = express.Router();
 
+const itemsPerPage = 25;
 
-router.get("/", async (req, res) => {
-    console.log("GET");
+
+router.get("/:page", async (req, res) => {
     try {
         const dbClient = await database.getClient();
         const flatProvider = providers.databaseFlatProvider(dbClient)
         const routerService = services.flatService(flatProvider);
-        const flats = await routerService.getFlats();
-        res.send(flats);
+
+        const page:number = Number.parseInt(req.params["page"]) ?? 0;
+
+        const flats = await routerService.getFlats({page, itemsPerPage});
+        res.status(200).send(flats);
     } catch (err:any){
-        res.send(err.message);
+        res.status(500).send(err.message);
     }
+});
+
+router.get("/maxPages", async (req, res) => {
+    const dbClient = await database.getClient();
+        const flatProvider = providers.databaseFlatProvider(dbClient)
+        const routerService = services.flatService(flatProvider);
+
+        const flats = await routerService.getFlats();
+
+        const pages = Math.ceil(flats.length / itemsPerPage);
+        res.status(200).send({pages});
 })
 
 export default router;
